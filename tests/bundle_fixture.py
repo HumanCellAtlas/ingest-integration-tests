30ef3c2660477c69558f029afc80a02b73d6efc9
Daniel Vaughan <dvaughan@ebi.ac.uk>
2018-06-12 13:02:51 +0100

    Add new integration test spreadsheet

--- tests/bundle_fixture.py
+++ tests/bundle_fixture.py
@@ -18,7 +18,7 @@ class BundleFixture:
 
     @property
     def metadata_spreadsheet_path(self):
-        xlsx_files = glob.glob(f"{self.bundle_path}/*_v5_*.xlsx")
+        xlsx_files = glob.glob(f"{self.bundle_path}/*_integration_test_*.xlsx")
         assert len(xlsx_files) == 1, f"There is more than 1 .xlsx file in {self.bundle_path}"
         return xlsx_files[0]
 
