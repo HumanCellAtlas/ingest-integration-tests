082a8c18c71aaf2b876c1dfac4fd38509beeaa67
Alegria Aclan <aaclan@ebi.ac.uk>
2019-03-13 15:41:15 +0000

    Adding initial codes

--- tests/test_end_to_end_dcp.py
+++ tests/test_end_to_end_dcp.py
@@ -6,10 +6,16 @@ import unittest
 
 from urllib.parse import urlparse
 
+import requests
+from ingest.utils.s2s_token_client import S2STokenClient
+from ingest.utils.token_manager import TokenManager
+
+from tests.analysis_submission_fixture import AnalysisSubmissionFixture
 from .utils import Progress
 from .wait_for import WaitFor
-from .ingest_agents import IngestUIAgent, IngestApiAgent
+from .ingest_agents import IngestUIAgent, IngestApiAgent, IngestAuthAgent
 from .dataset_fixture import DatasetFixture
+from ingest.api.ingestapi import IngestApi
 
 DEPLOYMENTS = ('dev', 'integration')
 
@@ -18,7 +24,10 @@ MINUTE = 60
 
 class DatasetRunner:
 
-    def __init__(self, deployment):
+    def __init__(self, deployment, export_bundles=False):
+        self._export_bundle = export_bundles
+        self.deployment = deployment
+
         self.ingest_broker = IngestUIAgent(deployment=deployment)
         self.ingest_api = IngestApiAgent(deployment=deployment)
         self.submission_id = None
@@ -26,7 +35,7 @@ class DatasetRunner:
         self.upload_credentials = None
         self.dataset = None
         self.upload_area_uuid = None
-        self.deployment = deployment
+        self.bundle_manifest = None
 
     def run(self, dataset_fixture):
         self.dataset = dataset_fixture
@@ -34,6 +43,20 @@ class DatasetRunner:
         self.get_upload_area_credentials()
         self.stage_data_files(dataset_fixture)
         self.wait_for_envelope_to_be_validated()
+        if self.export_bundles:
+            self.complete_submission()
+            self.wait_for_primary_and_results_bundles()
+
+    def complete_submission(self):
+        Progress.report("COMPLETING SUBMISSION...")
+        submit_url = self.submission_envelope.data['_links']['submit'][
+            'href']
+        headers = self.ingest_api.ingest_auth_agent.make_auth_header()
+        response = requests.put(submit_url, headers=headers)
+        if response.status_code != requests.codes.accepted:
+            raise RuntimeError(
+                f"PUT {submit_url} returned {response.status_code}: {response.content}")
+        Progress.report("  done.\n")
 
     def upload_spreadsheet_and_create_submission(self, bundle_fixture):
         spreadsheet_filename = os.path.basename(bundle_fixture.metadata_spreadsheet_path)
@@ -83,6 +106,12 @@ class DatasetRunner:
         Progress.report(f"envelope status is {envelope_status}")
         return envelope_status in ['Valid']
 
+    def _export_bundle(self):
+        pass
+
+    def find_assay_processes(self):
+        pass
+
     @staticmethod
     def _run_command(cmd_and_args_list, expected_retcode=0):
         retcode = subprocess.call(cmd_and_args_list)
@@ -94,6 +123,55 @@ class DatasetRunner:
             )
 
 
+class AnalysisSubmissionRunner:
+    def __init__(self, deployment):
+        self.deployment = deployment
+
+        self.ingest_api = IngestApi()
+        self.s2s_token_client = S2STokenClient()
+        gcp_credentials_file = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+        self.s2s_token_client.setup_from_file(gcp_credentials_file)
+        self.token_manager = TokenManager(token_client=self.s2s_token_client)
+
+        self.bundle_manifest = None
+        self.submission = None
+        self.analysis_fixture = AnalysisSubmissionFixture()
+
+    def create_secondary_submission(self, bundle_manifest):
+        self.bundle_manifest = bundle_manifest
+        token = self.token_manager.get_token()
+        submission_envelope = self.ingest_api.createSubmission(token)
+        self.submission = submission_envelope
+        submission_url = self.submission['_links']['self']['href']
+        process = self.ingest_api.createEntity(submission_url,
+                                     self.analysis_fixture.analysis_process,
+                                     'processes')
+        protocol = self.ingest_api.createEntity(submission_url,
+                                     self.analysis_fixture.analysis_protocol,
+                                     'protocols')
+
+        process_url = process['_links']['self']['href']
+        protocol_url = protocol['_links']['self']['href']
+
+        add_input_bundle_url = process['_links']['add-input-bundles']['href']
+        add_reference_files_url = process['add-file-reference']['href']
+
+        headers = { "Content-Type": "text/uri-list" }
+        r = requests.put(process_url, headers=headers, data=protocol_url)
+        r.raise_for_status()
+
+        input_bundle_uuid = self.bundle_manifest.get('bundleUuid')
+        bundle_refs_dict = {'bundleUuids': [input_bundle_uuid]}
+        r = requests.put(add_input_bundle_url, headers={'Content-type': 'application/json'},
+                                     json=bundle_refs_dict)
+        r.raise_for_status()
+
+        files = self.analysis_fixture.files
+
+        for file in files:
+
+
+
 class TestEndToEndDCP(unittest.TestCase):
 
     def setUp(self):
@@ -102,7 +180,7 @@ class TestEndToEndDCP(unittest.TestCase):
         if self.deployment not in DEPLOYMENTS:
             raise RuntimeError(f"CI_COMMIT_REF_NAME environment variable must be one of {DEPLOYMENTS}")
 
-    def ingest_store_and_analyze_bundle(self, dataset_name):
+    def ingest(self, dataset_name):
         dataset_fixture = DatasetFixture(dataset_name, self.deployment)
         runner = DatasetRunner(deployment=self.deployment)
         runner.run(dataset_fixture)
@@ -112,7 +190,7 @@ class TestEndToEndDCP(unittest.TestCase):
 class TestSmartSeq2Run(TestEndToEndDCP):
 
     def test_smartseq2_run(self):
-        runner = self.ingest_store_and_analyze_bundle('SS2')
+        runner = self.ingest('SS2')
 
 
 if __name__ == '__main__':
